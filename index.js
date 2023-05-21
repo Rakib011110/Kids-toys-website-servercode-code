5a537582733a4494e042f54e2d0f23eadd18367b
Rakibul Islam <rakib011110@gmail.com>
2023-05-21 09:29:27 +0600

    toys id add

--- index.js
+++ index.js
@@ -141,12 +141,7 @@ async function run() {
 
 
 
-        // app.get('/category/:id', async (req, res) => {
-        //     const id = req.params.id
-        //     const query = { _id: new ObjectId(id) }
-        //     const result = await toysCategoriesCollection.find(query).toArray()
-        //     res.send(result)
-        // })
+
 
         // app.get("/toyproduct", async (req, res) => {
 
